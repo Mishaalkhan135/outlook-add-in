4eb93897935ae323917b32f5bc2a2a8e0e0af503
Mishaal khan <mishaalkhan135@gmail.com>
2023-05-17 22:53:05 +0500

    Added the coincap.io api

--- My Office Add-in/src/taskpane/taskpane.js
+++ My Office Add-in/src/taskpane/taskpane.js
@@ -1,8 +1,9 @@
-// taskpane.js
-
 Office.onReady(() => {
   const reportButton = document.querySelector("#report-button");
   reportButton.addEventListener("click", sendEmail);
+
+  // Call the crypto API when the add-in loads
+  fetchCryptoData();
 });
 
 async function sendEmail() {
@@ -24,5 +25,28 @@ async function sendEmail() {
     ccRecipients: [],
     subject: subject,
     body: body,
+  });
+}
+
+async function fetchCryptoData() {
+  const response = await fetch("https://api.coincap.io/v2/assets");
+  const data = await response.json();
+  const cryptoTable = document.querySelector("#crypto-table tbody");
+
+  data.data.forEach((crypto, index) => {
+    const row = document.createElement("tr");
+    const rankCell = document.createElement("td");
+    const nameCell = document.createElement("td");
+    const priceCell = document.createElement("td");
+
+    rankCell.textContent = index + 1;
+    nameCell.textContent = crypto.name;
+    priceCell.textContent = parseFloat(crypto.priceUsd).toFixed(2);
+
+    row.appendChild(rankCell);
+    row.appendChild(nameCell);
+    row.appendChild(priceCell);
+
+    cryptoTable.appendChild(row);
   });
 }
