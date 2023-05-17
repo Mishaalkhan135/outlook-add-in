181f13e521e9e971c82c16512bdf9bb4212927cd
Mishaal khan <mishaalkhan135@gmail.com>
2023-05-17 23:36:55 +0500

    added the changed

--- My Office Add-in/src/taskpane/taskpane.js
+++ My Office Add-in/src/taskpane/taskpane.js
@@ -4,25 +4,16 @@ Office.onReady(() => {
   populateTable();
 });
 
-async function sendEmail() {
-  const item = Office.context.mailbox.item;
-
-  let from;
-  if (item && item.from && item.from.emailAddress) {
-    from = item.from.emailAddress;
-  } else {
-    console.error("Could not access the email sender's details");
-    return;
-  }
-
-  const subject = `Report: ${item.normalizedSubject}`;
-  const body = `This email was reported:\n\nSubject: ${item.normalizedSubject}\n\nFrom: ${from}`;
-
-  Office.context.mailbox.item.send({
+function sendEmail() {
+  Office.context.mailbox.displayNewMessageForm({
     toRecipients: ["mishaalkhan135@gmail.com"],
     ccRecipients: [],
-    subject: subject,
-    body: body,
+    subject: "Report",
+    body:
+      "This email was reported:\n\nSubject: " +
+      Office.context.mailbox.item.subject +
+      "\n\nFrom: " +
+      Office.context.mailbox.item.from.emailAddress,
   });
 }
 
